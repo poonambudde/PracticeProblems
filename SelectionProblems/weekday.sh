f12f1f7262f3d8ce24871f616c27ce95ac472780
“poonambudde” <buddepoonam123@gmail.com>
2022-01-16 21:53:44 +0530

    CaseStatement: Display the week day

---
+++ SelectionProblems/weekday.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+read -p  "Enter a number: " number
+
+case $number in
+
+1)
+  echo Sunday ;;
+2)
+  echo Monday ;;
+3)
+  echo Tuseday ;;
+4)
+  echo Wednesday ;;
+
+5)
+  echo Thursday ;;
+
+6)
+  echo Friday ;;
+
+7)
+  echo Saturday ;;
+
+esac
